71aa0eaa42b14744ee443ebc97071696b81ab6cb
OstapSalo <140599370+OstapSalo@users.noreply.github.com>
2024-01-08 00:16:08 +0200

    - hot fix v2

--- StarWars/metro.config.js
+++ StarWars/metro.config.js
@@ -3,6 +3,12 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 const {assetExts, sourceExts} = defaultConfig.resolver;
 
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 const config = {
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
@@ -13,4 +19,4 @@ const config = {
   },
 };
 
-export default mergeConfig(defaultConfig, config);
+module.exports = mergeConfig(defaultConfig, config);
